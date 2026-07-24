ddcdab8144fa24c41dcc94e1ae94864e67d2657f
XiaoQuark <kris.oldrini@gmail.com>
2026-07-24 10:30:00 +0100

    fix: cat script 3 and sed script 1

--- individual-shell-tools/cat/script-03.sh
+++ individual-shell-tools/cat/script-03.sh
@@ -10,18 +10,3 @@ set -euo pipefail
 # 2 I was tempted to take a bite of it.
 # 3 But this seemed like a bad idea...
 cat -n ../helper-files/helper-3.txt
-
-#!/bin/bash
-
-set -euo pipefail
-
-# TODO: Write a command to output the contents of all of the files inside the helper-files directory to the terminal.
-# Make sure you are only calling `cat` once.
-#
-# The output of this command should be:
-# Once upon a time...
-# There was a house made of gingerbread.
-# It looked delicious.
-# I was tempted to take a bite of it.
-# But this seemed like a bad idea...
-cat ../helper-files/*.txt
